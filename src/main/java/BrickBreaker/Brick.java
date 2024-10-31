301c261667884263b28757ab0f32e8965f952be0
Yael Green <118456307+YCGreen@users.noreply.github.com>
2024-10-30 20:00:35 -0400

    initial commit

---
+++ src/main/java/BrickBreaker/Brick.java
@@ -0,0 +1,14 @@
+package BrickBreaker;
+
+import java.awt.*;
+
+public class Brick extends Rectangle {
+
+    public Brick(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+
+
+
+}
